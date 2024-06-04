5faaafbaf4e5955777d6e7c1b6500947c6913b83
bsester <124089310+bsester@users.noreply.github.com>
2024-06-04 14:18:48 -0500

    routing to homepage, blank homepage

--- my-investor/src/App.js
+++ my-investor/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Header"
 import {HashRouter, Route, Routes} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Home from "./Home";
 
 function App() {
   return (
@@ -9,7 +12,7 @@ function App() {
         <HashRouter>
           <Header/>
           <Routes>
-            {/*<Route path = '/' exact element={<Home/>}/>*/}
+            <Route path = '/' exact element={<Home/>}/>
             {/*<Route path = '/aboutme' element={<AboutMe/>}/>*/}
             {/*<Route path = '/projects' element={<Projects/>}/>*/}
             {/*<Route path = '/education' element = {<Education/>}/>*/}
